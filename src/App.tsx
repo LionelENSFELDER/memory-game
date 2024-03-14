43d3d742a894d89664db6694f0f0741a9bd24c73
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2024-03-14 20:00:31 +0100

    add reload game button

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,14 @@ function App() {
 
   return (
     <>
+      <button
+        onClick={() => window.location.reload(false)}
+        className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+      >
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+          Reload game
+        </span>
+      </button>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {currentCards.map((card, index) => {
           return (
