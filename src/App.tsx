a4f710f0b73474d09a39463047b6d9dfea03dd45
ENSFELDER Lionel <ensfelder.lionel@gmail.com>
2024-03-17 12:16:44 +0100

    add navbar, lucide icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { defaultCardsData } from "./data/default-cards";
 import Card from "./components/card";
+import Navbar from "./components/navbar";
 import "./App.css";
 
 function App() {
@@ -46,14 +47,7 @@ function App() {
 
   return (
     <>
-      <button
-        onClick={() => window.location.reload(false)}
-        className="relative inline-flex items-center justify-center p-1 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-      >
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-          Reload game
-        </span>
-      </button>
+      <Navbar />
       <div className="flex justify-center align-middle">
         <div className="p-8 w-fit grid grid-cols-2 md:grid-cols-4 gap-2">
           {currentCards.map((card, index) => {
